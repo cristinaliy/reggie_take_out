9c1b09f435c19b0c112a0dce6e0ba4b3b853d902
vici <468606362@qq.com>
2023-05-31 15:41:35 +0800

    加入redis，缓存菜品

---
+++ src/main/java/com/heima/Main.java
@@ -0,0 +1,14 @@
+package com.heima;
+
+/**
+ * ClassName:${NAME}
+ * Package:com.heima
+ * Description:
+ *
+ * @Author:Vici Create:${DATE} - ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
